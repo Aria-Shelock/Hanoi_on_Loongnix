47e41f021e9297e8397b90947135adc46e8f3d65
Aria-Shelock <84490374+Aria-Shelock@users.noreply.github.com>
2021-09-28 22:17:22 +0800

    Delete camshot-master/camshot-master directory

--- camshot-master/camshot-master/cam.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-camshot -W 640 -H 480 -p nenew_shot &
-sleep 5
-cat nenew_shot>/home/loongson/2021/pic/camshot.bmp
-kill $(ps aux |grep camshot |grep -v grep|awk '{print$2}')
--- camshot-master/camshot-master/cam1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-camshot -W 640 -H 480 -p nenew_shot &
-sleep 5
-cat nenew_shot>camshot.bmp
